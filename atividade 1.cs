cd67eaf53d85bd8284056f2243a639780a9fe017
Augusto Menezes Freitas <96602340+augusto-menezes-freitas@users.noreply.github.com>
2024-08-25 22:35:11 -0300

    Add files via upload

---
+++ atividade 1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double l;
+        Console.WriteLine("Digite o valor do lado ");
+        l = double.Parse(Console.ReadLine());
+        double p = 4 * l;
+        double a = l * l;
+        double d = l * Math.Sqrt(2);
+        Console.WriteLine("perimetro é " + p);
+        Console.WriteLine("área é " + a);
+        Console.WriteLine("diagonal é " + d);
+        Console.ReadLine();
+     }
+}
+
